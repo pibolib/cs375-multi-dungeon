736b00c2d6cbdc91d5b675f2f2220f4d67ef99db
Jonathan Ouf <pibolib@gmail.com>
2024-11-20 10:35:17 -0500

    Ladies, gentlemen, all in between and otherwise, we have movement

--- app/server.js
+++ app/server.js
@@ -23,17 +23,11 @@ let hostname = "localhost";
 let tokenStorage = {};
 
 pool.connect().then(() => {
-    console.log("Connected to the database!");
+	console.log("Connected to the database!");
 });
 
 // Entities in the game world
-let entities = [
-    {
-        entityType: "dummy",
-        posX: 4,
-        posY: 5,
-    },
-];
+let entities = [];
 
 // Queue of update events
 let updateEvents = [];
@@ -42,7 +36,7 @@ let updateEvents = [];
 let clients = new Map();
 
 app.post("/test", (req, res) => {
-    console.log(req.body);
+	console.log(req.body);
 });
 
 LoginLogout(app, pool, tokenStorage);
@@ -50,136 +44,137 @@ LoginLogout(app, pool, tokenStorage);
 // Create HTTP server and attach the WebSocket server to it
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
-    console.log(`Server running on http://${hostname}:${port}`);
+	console.log(`Server running on http://${hostname}:${port}`);
 });
 
 // Set up the WebSocket server
 const wss = new ws.WebSocketServer({ server });
 wss.on("connection", (client) => {
-    console.log("New client connected!");
+	console.log("New client connected!");
 
-    // Send current entities to the new client
-    let clientUpdateMessage = {
-        messageType: "refresh",
-        messageBody: entities,
-    };
-    client.send(JSON.stringify(clientUpdateMessage));
+	// Send current entities to the new client
+	let clientUpdateMessage = {
+		messageType: "refresh",
+		messageBody: entities,
+	};
+	client.send(JSON.stringify(clientUpdateMessage));
 
-    // Create a new entity for the player
-    let newEntity = {
-        entityType: "player",
-        posX: Math.floor(Math.random() * 8),
-        posY: Math.floor(Math.random() * 8),
-    };
-    let newEntityMessage = {
-        messageType: "spawn",
-        messageBody: newEntity,
-    };
+	// Create a new entity for the player
+	let newEntity = {
+		entityType: "player",
+		posX: Math.floor(Math.random() * 8),
+		posY: Math.floor(Math.random() * 8),
+	};
+	let newEntityMessage = {
+		messageType: "spawn",
+		messageBody: newEntity,
+	};
 	clients.set(client, { id: entities.length, action: "none" });
 	entities.push(newEntity);
 
-    updateEvents.push(JSON.stringify(newEntityMessage));
+	updateEvents.push(JSON.stringify(newEntityMessage));
 
-    // Handle messages from the client
-    client.on("message", (message) => {
-        try {
-            let messageObject = JSON.parse(message);
-            console.log(`New message: ${message}`);
-            let clientData = clients.get(client);
+	// Handle messages from the client
+	client.on("message", (message) => {
+		try {
+			let messageObject = JSON.parse(message);
+			console.log(`New message: ${message}`);
+			let clientData = clients.get(client);
 
-            if (messageObject.messageType == "chat") {
-                broadcast(message);
-            } else if (clientData) {
-                switch (messageObject.messageType) {
-                    case "moveLeft":
-                        clientData.action = "moveLeft";
-                        break;
-                    case "moveRight":
-                        clientData.action = "moveRight";
-                        break;
-                    case "moveUp":
-                        clientData.action = "moveUp";
-                        break;
-                    case "moveDown":
-                        clientData.action = "moveDown";
-                        break;
-                }
-            }
-        } catch (error) {
-            console.error("Error processing message:", error);
-        }
-    });
+			if (messageObject.messageType == "chat") {
+				broadcast(message);
+			} else if (clientData) {
+				switch (messageObject.messageType) {
+					case "moveLeft":
+						clientData.action = "moveLeft";
+						break;
+					case "moveRight":
+						clientData.action = "moveRight";
+						break;
+					case "moveUp":
+						clientData.action = "moveUp";
+						break;
+					case "moveDown":
+						clientData.action = "moveDown";
+						break;
+				}
+				console.log(`Client ${client} action: ${clientData.action}`);
+			}
+		} catch (error) {
+			console.error("Error processing message:", error);
+		}
+	});
 
-    client.on("close", () => {
-        let clientData = clients.get(client);
+	client.on("close", () => {
+		let clientData = clients.get(client);
 
-        if (clientData) {
-            let despawnMessage = {
-                messageType: "despawn",
-                messageBody: { id: clientData.id },
-            };
-
-            updateEvents.push(JSON.stringify(despawnMessage));
-            entities[clientData.id] = { 
-				entityType: "none", 
+		if (clientData) {
+			let despawnMessage = {
+				messageType: "despawn",
+				messageBody: { id: clientData.id },
 			};
-            clients.delete(client);
-        }
-    });
+
+			updateEvents.push(JSON.stringify(despawnMessage));
+			entities[clientData.id] = {
+				entityType: "none",
+			};
+			clients.delete(client);
+		}
+	});
 });
 
 // Sends message to every client that is currently active
 function broadcast(message) {
-    for (let client of wss.clients) {
-        if (client.readyState === ws.OPEN) {
-            client.send(message);
-        }
-    }
+	for (let client of wss.clients) {
+		if (client.readyState === ws.OPEN) {
+			client.send(message);
+		}
+	}
 }
 
 function handleCycle() {
-    for (let [client, clientData] of clients.entries()) {
-        if (clientData.id === -1) {
-            continue;
-        }
+	for (let [client, clientData] of clients.entries()) {
+		if (clientData.id === -1) {
+			continue;
+		}
 
-        let entity = entities[clientData.id];
-        let didAction = false;
+		let entity = entities[clientData.id];
+		let didAction = false;
 
-        switch (clientData.action) {
-            case "moveLeft":
-                entity.posX -= 1;
-                didAction = true;
-                break;
-            case "moveRight":
-                entity.posX += 1;
-                didAction = true;
-                break;
-            case "moveUp":
-                entity.posY -= 1;
-                didAction = true;
-                break;
-            case "moveDown":
-                entity.posY += 1;
-                didAction = true;
-                break;
-        }
-        clientData.action = "none";
-        if (didAction) {
-            let actionEvent = {
-                messageType: "updateStatus",
-                messageBody: {
-                    actor: clientData.id,
-                    newState: entity,
-                },
-            };
-            updateEvents.push(JSON.stringify(actionEvent));
-        }
-    }
-    for (let i = 0; i < updateEvents.length; i++) {
+		switch (clientData.action) {
+			case "moveLeft":
+				entity.posX -= 1;
+				didAction = true;
+				break;
+			case "moveRight":
+				entity.posX += 1;
+				didAction = true;
+				break;
+			case "moveUp":
+				entity.posY -= 1;
+				didAction = true;
+				break;
+			case "moveDown":
+				entity.posY += 1;
+				didAction = true;
+				break;
+		}
+		clientData.action = "none";
+		if (didAction) {
+			let actionEvent = {
+				messageType: "updateStatus",
+				messageBody: {
+					actor: clientData.id,
+					newState: entity,
+				},
+			};
+			updateEvents.push(JSON.stringify(actionEvent));
+		}
+	}
+	for (let i = 0; i < updateEvents.length; i++) {
 		broadcast(updateEvents[i]);
 	}
-    updateEvents = [];
+	updateEvents = [];
 }
 
 setInterval(handleCycle, 1000);
--- app/web/game.js
+++ app/web/game.js
@@ -1,69 +1,83 @@
 const ws = new WebSocket(`ws://${window.document.location.host}`);
-const players = [];
+let players = [];
 const app = new PIXI.Application();
 
 app.init({
-    backgroundColor: "#1099bb",
-    resizeTo: window
-})
-.then(() => {
-    document.body.appendChild(app.view);
-    setUp();
+	backgroundColor: "#1099bb",
+	width: 500,
+	height: 500,
+}).then(() => {
+	document.body.appendChild(app.view);
+	setUp();
 });
 
 function setUp() {
-    // Sending an update to the server when an arrow key is pressed
-    document.addEventListener("keydown", (event) => {
-        const key = event.key;
-        let message = {
-            messageType: "chat",
-        };
+	// Sending an update to the server when an arrow key is pressed
+	document.addEventListener("keydown", (event) => {
+		const key = event.key;
+		let message = {};
 
-        switch (key) {
-            case "ArrowLeft":
-                message.messageType = "moveLeft";
-                break;
-            case "ArrowRight":
-                message.messageType = "moveRight";
-                break;
-            case "ArrowUp":
-                message.messageType = "moveUp";
-                break;
-            case "ArrowDown":
-                message.messageType = "moveDown";
-                break;
-        }
+		switch (key) {
+			case "ArrowLeft":
+				message.messageType = "moveLeft";
+				break;
+			case "ArrowRight":
+				message.messageType = "moveRight";
+				break;
+			case "ArrowUp":
+				message.messageType = "moveUp";
+				break;
+			case "ArrowDown":
+				message.messageType = "moveDown";
+				break;
+		}
 
-        ws.send(JSON.stringify(message));
-    });
+		if (message.hasOwnProperty("messageType")) {
+			ws.send(JSON.stringify(message));
+		}
+	});
 
-    ws.addEventListener("message", (event) => {
-        let message = JSON.parse(event.data);
-        if (message != null) {
-            updateGame(message);
-        }
-        console.log(message);
-    });
+	ws.addEventListener("message", (event) => {
+		let message = JSON.parse(event.data);
+		if (message != null) {
+			updateGame(message);
+		}
+		console.log("Message received:", message);
+	});
 }
 
 function updateGame(message) {
-    console.log("Message received:", message);
-    switch (message.messageType) {
-        case "spawn":
-            console.log("Spawning player:", message.messageBody);
-            createPlayer(message.messageBody);
-            break;
-    }
+	switch (message.messageType) {
+		case "spawn":
+			console.log("Spawning player:", message.messageBody);
+			createPlayer(message.messageBody);
+			break;
+		case "updateStatus":
+			players[message.messageBody.actor].x =
+				message.messageBody.newState.posX * 50;
+			players[message.messageBody.actor].y =
+				message.messageBody.newState.posY * 50;
+			break;
+		case "refresh":
+			app.stage.removeChildren();
+			players = [];
+			for (let newEntity of message.messageBody) {
+				createPlayer(newEntity);
+			}
+
+		// TODO!
+	}
 }
 
 async function createPlayer(messageBody) {
-    if (messageBody.entityType === "player") {
-        console.log("yes");
-        let sprite = new PIXI.Sprite(PIXI.Texture.from("images/bunny.png"));
-        sprite.anchor.set(0.5);
-        sprite.x = messageBody.posX * 50;
-        sprite.y = messageBody.posY * 50;
-        app.stage.addChild(sprite);
-        players.push(sprite);
-    }
+	if (messageBody.entityType === "player") {
+		console.log("yes");
+		let texture = await PIXI.Assets.load("assets/bunny.png");
+		let sprite = new PIXI.Sprite(texture);
+		sprite.anchor.set(0.5);
+		sprite.x = messageBody.posX * 50;
+		sprite.y = messageBody.posY * 50;
+		app.stage.addChild(sprite);
+		players.push(sprite);
+	}
 }
